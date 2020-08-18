22e902b834b349d7d5867d0325c55a61311f67df
Shubh Gupta <shubhngupta04@gmail.com>
2020-08-18 11:08:00 +0530

    blog detail page added

--- viitblogs/blogs/urls.py
+++ viitblogs/blogs/urls.py
@@ -5,6 +5,7 @@ app_name = 'blogs'
 
 urlpatterns = [
     path('blog_list/',views.blog_list,name="blog_list"),
-    
+    path('blog_detail/<slug:slug>/',views.blog_detail.as_view(),name="blog_detail"),
+
     # path('home/',views.index,name="home"),
 ]
--- viitblogs/blogs/views.py
+++ viitblogs/blogs/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from .models import Blogs
+from django.views import generic
+from django.views.generic import  (View,TemplateView,
+                                  ListView,DetailView,
+                                  CreateView,UpdateView,
+                                  DeleteView)
 
 # Create your views here.
 def index(request):
@@ -8,3 +13,11 @@ def index(request):
 def blog_list(request):
     bloglist = Blogs.objects.all()
     return render(request,'blogs/blog_list.html',{'bloglist':bloglist})
+
+class blog_detail(DetailView):
+    context_object_name = 'blog'
+    model = Blogs
+    template_name = 'blogs/blog_detail.html'
+
+def upvote(request,slug):
+    
