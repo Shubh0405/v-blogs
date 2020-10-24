634eeecc323bbff8e7a24e289eb30c41a675b1bd
Shubh Gupta <shubhngupta04@gmail.com>
2020-10-24 15:56:13 +0530

    true

--- viitblogs/settings.py
+++ viitblogs/settings.py
@@ -27,7 +27,7 @@ MEDIA_DIR = os.path.join(BASE_DIR,'media')
 SECRET_KEY = 'eb5ee18%3)#(*9)v2k=v0+3#x7=xz-5a6e#q*t&ya)o79=a+3='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['v-blogs.herokuapp.com','127.0.0.1']
 
